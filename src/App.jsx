5c70d6ae31a8a82ce03eac5c8669aa70d820b42a
Cássio Pereira <cassiorodp@gmail.com>
2022-07-30 15:10:34 -0300

    feat: add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import './App.css';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
 import Login from './pages/login/Login.page';
 import theme from './theme';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <ThemeProvider theme={theme}>
-        <Login />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Navigate replace to="/login" />} />
+        </Routes>
       </ThemeProvider>
-    </div>
+    </Router>
   );
 }
 
