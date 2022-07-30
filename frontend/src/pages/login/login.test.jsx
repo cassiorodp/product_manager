52f24f0fa8b808b2bf808afa060fa4c8273de041
Cássio Pereira <cassiorodp@gmail.com>
2022-07-30 18:23:31 -0300

    test: update login

--- frontend/src/pages/login/login.test.jsx
+++ frontend/src/pages/login/login.test.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { render, screen } from '../../test-utils/testing-library-utils';
 import Login from './Login.page';
+import api from '../../api';
 
 describe('Login page', () => {
   it('should render all components', () => {
     render(<Login />);
 
     const loginImage = screen.getByRole('img');
-    const loginHeading = screen.getByRole('heading', { name: /general store/i, level: 1 });
+    const loginHeading = screen.getByRole('heading', {
+      name: /general store/i,
+      level: 1,
+    });
     const emailInput = screen.getByLabelText(/email/i);
     const passwordInput = screen.getByLabelText(/password/i);
 
@@ -17,15 +21,24 @@ describe('Login page', () => {
     expect(passwordInput).toBeInTheDocument();
   });
 
-  it('should have working inputs', async () => {
+  it('should set an error message when login fails', async () => {
     const { user } = render(<Login />);
+    const apiSpy = jest.spyOn(api, 'postLogin').mockResolvedValue(false);
     const emailInput = screen.getByLabelText(/email/i);
     const passwordInput = screen.getByLabelText(/password/i);
 
-    await user.type(emailInput, 'user@mail.com');
-    await user.type(passwordInput, 'secretpassword');
+    await user.type(emailInput, 'test@mail.com');
+    await user.type(passwordInput, '123456');
 
-    expect(emailInput).toHaveValue('user@mail.com');
-    expect(passwordInput).toHaveValue('secretpassword');
+    const submitButton = screen.getByText(/login/i);
+
+    await user.click(submitButton);
+    expect(apiSpy).toHaveBeenCalledWith({
+      email: 'test@mail.com',
+      password: '123456',
+    });
+
+    const loginError = screen.getByText('email and/or password is invalid');
+    expect(loginError).toBeInTheDocument();
   });
 });
