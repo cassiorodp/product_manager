ef6d3108441b0a5ea145a8a5be5e30df9f75b555
Cássio Pereira <cassiorodp@gmail.com>
2022-07-30 18:36:53 -0300

    feat: add Main route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import {
 } from 'react-router-dom';
 import Login from './pages/login/Login.page';
 import theme from './theme';
+import Main from './pages/Main/Main.page';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <ThemeProvider theme={theme}>
         <Routes>
           <Route path="/login" element={<Login />} />
+          <Route path="/products" element={<Main />} />
           <Route path="/" element={<Navigate replace to="/login" />} />
         </Routes>
       </ThemeProvider>
