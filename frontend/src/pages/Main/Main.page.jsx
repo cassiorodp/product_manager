ad3ae7dcf24e4a5c79475f7e3e17e9e5ece8180a
Cássio Pereira <cassiorodp@gmail.com>
2022-07-30 18:37:15 -0300

    feat: add Main page

---
+++ frontend/src/pages/Main/Main.page.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ProductsTable from '../../components/products_table/ProductsTable.component';
+
+export default function Main() {
+  return (
+    <ProductsTable />
+  );
+}
