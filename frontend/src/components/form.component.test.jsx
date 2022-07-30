e3bcd3917afe16a9646825d945f10ac3a58f6082
Cássio Pereira <cassiorodp@gmail.com>
2022-07-30 18:15:00 -0300

    test: update form component

--- frontend/src/components/form.component.test.jsx
+++ frontend/src/components/form.component.test.jsx
@@ -3,6 +3,17 @@ import { render, screen } from '../test-utils/testing-library-utils';
 import Form from './form.component';
 
 describe('Form component', () => {
+  it('should have working inputs', async () => {
+    const { user } = render(<Form />);
+    const emailInput = screen.getByLabelText(/email/i);
+    const passwordInput = screen.getByLabelText(/password/i);
+
+    await user.type(emailInput, 'user@mail.com');
+    await user.type(passwordInput, 'secretpassword');
+
+    expect(emailInput).toHaveValue('user@mail.com');
+    expect(passwordInput).toHaveValue('secretpassword');
+  });
   it('should render errors and not call submit when passed invalid data', async () => {
     const mockSubmit = jest.fn();
     const { user } = render(<Form onSubmit={mockSubmit} />);
@@ -21,7 +32,9 @@ describe('Form component', () => {
   it('should render a login error when its true', () => {
     render(<Form loginError />);
 
-    const loginErrorMessage = screen.getByText('email and/or password is invalid');
+    const loginErrorMessage = screen.getByText(
+      'email and/or password is invalid',
+    );
     expect(loginErrorMessage).toBeInTheDocument();
   });
 });
