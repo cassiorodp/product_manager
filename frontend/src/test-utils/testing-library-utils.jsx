00a5ba2f4ee52c80c194a5a09c6e06e4254684e8
Cássio Pereira <cassiorodp@gmail.com>
2022-07-30 18:15:18 -0300

    chore: update testing library utils

--- frontend/src/test-utils/testing-library-utils.jsx
+++ frontend/src/test-utils/testing-library-utils.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { render as rtlRender } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { ThemeProvider } from 'styled-components';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import theme from '../theme';
 
 const render = (ui, options) => {
   function Wrapper({ children }) {
     return (
-      <Router>
+      <MemoryRouter>
         <ThemeProvider theme={theme}>{children}</ThemeProvider>
-      </Router>
+      </MemoryRouter>
     );
   }
   return {
