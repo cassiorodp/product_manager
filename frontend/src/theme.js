b3500f4547d1a4dc42faac5f7713854ba35eb05b
Cássio Pereira <cassiorodp@gmail.com>
2022-08-01 19:20:23 -0300

    feat: add new colors

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,9 +1,12 @@
 const theme = {
   colors: {
     background1: '#ced4da',
+    background1Dark: '#6c757d',
     colorPrimary: '#55c57a',
     colorPrimaryDark: '#40916c',
     colorError: '#d00000',
+    colorSecondary: '#6495ED',
+    colorSecondaryDark: '#0069D9',
   },
 };
 
