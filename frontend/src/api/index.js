405d19a42e9236e466f5123d54821684b81a3b97
Cássio Pereira <cassiorodp@gmail.com>
2022-08-01 12:10:02 -0300

    feat: add api postProduct function

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -7,20 +7,37 @@ const postLogin = async (data) => {
     const response = await axios.post(`${BASE_URL}/auth/login`, data);
     return response.data.accessToken;
   } catch (error) {
+    console.log(error);
     return null;
   }
 };
 
 const getProducts = async (page, sortParam) => {
   try {
+    const accessToken = JSON.parse(localStorage.getItem('accessToken'));
     const response = await axios.get(
       `${BASE_URL}/products?_page=${page}&_sort=${sortParam}`,
+      { headers: { authorization: accessToken } },
     );
     return {
       data: response.data,
       totalCount: response.headers['x-total-count'],
     };
   } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+const postProduct = async (productJson) => {
+  try {
+    const accessToken = JSON.parse(localStorage.getItem('accessToken'));
+    const response = await axios.post(`${BASE_URL}/products`, productJson, {
+      headers: { authorization: accessToken },
+    });
+    return response;
+  } catch (error) {
+    console.log(error);
     return null;
   }
 };
@@ -28,4 +45,5 @@ const getProducts = async (page, sortParam) => {
 export default {
   postLogin,
   getProducts,
+  postProduct,
 };
