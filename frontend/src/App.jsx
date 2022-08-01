d6516f604036a1b384caaa5107ebd58483f3a78a
Cássio Pereira <cassiorodp@gmail.com>
2022-08-01 10:40:23 -0300

    feat: setup store & provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,21 +7,25 @@ import {
   Route,
   Routes,
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Login from './pages/login/Login.page';
 import theme from './theme';
 import Main from './pages/Main/Main.page';
+import store from './redux/store';
 
 function App() {
   return (
     <Router>
-      <ThemeProvider theme={theme}>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/products" element={<Main />} />
-          <Route path="/" element={<Navigate replace to="/login" />} />
-          <Route path="*" element={<Navigate replace to="/login" />} />
-        </Routes>
-      </ThemeProvider>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/products" element={<Main />} />
+            <Route path="/" element={<Navigate replace to="/login" />} />
+            <Route path="*" element={<Navigate replace to="/login" />} />
+          </Routes>
+        </ThemeProvider>
+      </Provider>
     </Router>
   );
 }
