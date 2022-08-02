d6e7c4798da6cc21ad4a825f26ef573364308ceb
Cássio Pereira <cassiorodp@gmail.com>
2022-08-02 19:36:29 -0300

    feat: update productTable

--- frontend/src/components/products_table/ProductsTable.component.jsx
+++ frontend/src/components/products_table/ProductsTable.component.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { FiCheck, FiX } from 'react-icons/fi';
+import { BsFillTrashFill } from 'react-icons/bs';
+import { BiEdit } from 'react-icons/bi';
 import mixins from '../../mixins';
+import { ActionButton, ButtonCell, Table } from './productsTable.style';
 
-const { getFormatedDate } = mixins;
+const { getFormatedDate, getFormatedCurrency } = mixins;
 
 export default function ProductsTable({ products }) {
   return (
-    <table>
+    <Table>
       <thead>
         <tr>
           <th>Name</th>
@@ -25,11 +28,14 @@ export default function ProductsTable({ products }) {
               <td>{getFormatedDate(product.fabDate)}</td>
               <td>{product.perishable ? <FiCheck /> : <FiX />}</td>
               <td>{getFormatedDate(product.expDate)}</td>
-              <td>{product.price}</td>
-              <td />
+              <td>{`R$ ${getFormatedCurrency(product.price)}`}</td>
+              <ButtonCell>
+                <ActionButton edit><BiEdit /></ActionButton>
+                <ActionButton delete><BsFillTrashFill /></ActionButton>
+              </ButtonCell>
             </tr>
           ))}
       </tbody>
-    </table>
+    </Table>
   );
 }
