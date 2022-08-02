1f7eaa8f92138be994c9162785c50631c18ca72c
Cássio Pereira <cassiorodp@gmail.com>
2022-08-02 19:37:30 -0300

    feat: update table container

--- frontend/src/components/table_container/TableContainer.component.jsx
+++ frontend/src/components/table_container/TableContainer.component.jsx
@@ -2,8 +2,10 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getProductsPerPage } from '../../redux/async_actions/productActions';
 import { selectProduct } from '../../redux/slices/productSlice';
+import ConfigurationButtons from '../configuration_buttons/ConfigurationButtons.component';
 import PaginateButtons from '../paginate_buttons/PaginateButtons.component';
 import ProductsTable from '../products_table/ProductsTable.component';
+import Container from './TableContainer.style';
 
 export default function TableContainer() {
   const {
@@ -15,9 +17,10 @@ export default function TableContainer() {
   }, []);
 
   return (
-    <>
+    <Container>
+      <ConfigurationButtons />
       <ProductsTable products={products} />
       <PaginateButtons />
-    </>
+    </Container>
   );
 }
