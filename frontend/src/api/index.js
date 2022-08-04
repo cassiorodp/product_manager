b73f6c02dbb11f0a62eb8db0e25424b892629262
Cássio Pereira <cassiorodp@gmail.com>
2022-08-03 22:01:19 -0300

    chore: add api url env var

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://localhost:3001';
+export const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
 
 const postLogin = async (data) => {
   try {
