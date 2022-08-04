f44b3fe241c7520fe93a417fd5840b644547db8a
Cássio Pereira <cassiorodp@gmail.com>
2022-08-04 08:53:32 -0300

    fix: update handlers

--- frontend/src/mocks/handlers.js
+++ frontend/src/mocks/handlers.js
@@ -3,11 +3,11 @@ import { BASE_URL } from '../api';
 import products from './mockResponses';
 
 const handlers = [
-  rest.post(`${BASE_URL}/auth/login`, (req, res, ctx) => res(ctx.status(200), ctx.json('fakeToken'))),
-  rest.get(`${BASE_URL}/products`, (req, res, ctx) => res(ctx.status(200), ctx.json(products))),
-  rest.post(`${BASE_URL}/products`, (req, res, ctx) => res(ctx.status(201))),
-  rest.put(`${BASE_URL}/products/:id`, (req, res, ctx) => res(ctx.status(200))),
-  rest.delete(`${BASE_URL}/products/:id`, (req, res, ctx) => res(ctx.status(200))),
+  rest.post(`${BASE_URL}auth/login`, (req, res, ctx) => res(ctx.status(200), ctx.json('fakeToken'))),
+  rest.get(`${BASE_URL}products`, (req, res, ctx) => res(ctx.status(200), ctx.json(products))),
+  rest.post(`${BASE_URL}products`, (req, res, ctx) => res(ctx.status(201))),
+  rest.put(`${BASE_URL}products/:id`, (req, res, ctx) => res(ctx.status(200))),
+  rest.delete(`${BASE_URL}products/:id`, (req, res, ctx) => res(ctx.status(200))),
 ];
 
 export default handlers;
